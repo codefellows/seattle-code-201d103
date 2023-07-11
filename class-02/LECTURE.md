3e8ac0a73d49e88237409f17f570a3867a791434
JacobKnaack <jacobknck@gmail.com>
2023-07-11 12:18:24 -0700

    adds lecture notes and demo code

---
+++ class-02/LECTURE.md
@@ -0,0 +1,5 @@
+# Class 02 Lecture Notes
+
+[Virtual Whiteboard](https://projects.invisionapp.com/freehand/document/qH33Qzb68)
+[Class 01 deployed demo](https://jacobknaack.me/class-01-demo/)
+[Class 01 demo code](https://github.com/JacobKnaack/class-01-demo)
--- class-02/typed-demo/app.js
+++ class-02/typed-demo/app.js
@@ -1,25 +1,24 @@
 'use strict';
 
+let name = prompt('What is you name?');
+if (name) {
+  alert('Thats a great name!');
+} else if (name === null) {
+  alert('Oh I see');
+}
 
-// let name = prompt('What is you name?');
-// if (name) {
-//   alert('Thats a great name!');
-// } else if (name === null) {
-//   alert('Oh I see');
-// }
 
+let age = prompt('What is your age?');
+if (age > 30) {
+  alert('What a great Age!');
+}
 
-// let age = prompt('What is your age?');
-// if (age > 30) {
-//   alert('What a great Age!');
-// }
-
-// let lovesJs = prompt('Do you love JS?');
-// if (lovesJs) {
-//   alert('Me Too!')
-// } else {
-//   alert('Aww too bad');
-// }
+let lovesJs = prompt('Do you love JS?');
+if (lovesJs) {
+  alert('Me Too!')
+} else {
+  alert('Aww too bad');
+}
 
 let foodPreference = prompt('What is your favorite food?');
 switch(foodPreference.toLowerCase()) { // normalizing our user input to all lower case characters.
